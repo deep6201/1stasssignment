95462782dae1292085f2f71a5014ab730dd80f47
deep6201 <90336103+deep6201@users.noreply.github.com>
2021-11-02 14:33:56 +0530

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,1 +1,7 @@
-console.log("myself deepanshu");
+var a="HELLO"
+var y = "DEEPANSHU TIBREWAL";
+var x = "this JS file is linked with HTML page";
+var z = y+" "+ x;
+console.log(x);
+console.log(y);
+console.log(z);
